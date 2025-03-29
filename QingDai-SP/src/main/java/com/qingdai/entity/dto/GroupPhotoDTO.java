a17ca6d8e25e602815a85e6e439479bdb07bce45
LiuZiMing <3335308825@qq.com>
2025-03-29 22:08:12 +0800

    核心：所有id变为string  添加组图与照片的关系表及相关逻辑

--- QingDai-SP/src/main/java/com/qingdai/entity/dto/GroupPhotoDTO.java
+++ QingDai-SP/src/main/java/com/qingdai/entity/dto/GroupPhotoDTO.java
@@ -1,11 +1,12 @@
 package com.qingdai.dto;
 
 import com.qingdai.entity.GroupPhoto;
-import com.qingdai.entity.Photo;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class GroupPhotoDTO {
     public GroupPhoto groupPhoto;
-    public Photo photo;
+    public List<Long> PhotoIds;
 }
